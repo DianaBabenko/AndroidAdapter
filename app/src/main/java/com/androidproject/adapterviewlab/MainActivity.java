bcba6239c757cec615b8e378822cddb4b3b8f3f5
DianaBabenko <baben.k.o.diana2017@gmail.com>
2019-12-06 02:52:50 +0200

    normal version

--- app/src/main/java/com/androidproject/adapterviewlab/MainActivity.java
+++ app/src/main/java/com/androidproject/adapterviewlab/MainActivity.java
@@ -2,18 +2,24 @@ package com.androidproject.adapterviewlab;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText nameEditText;
+    private EditText markEditText;
     ArrayList<Student> students = new ArrayList();
     //ArrayAdapter<String> adapter;
-
-
+    StudentAdapter adapter;
+    Student studentItem;
     ListView studentList;
+
+
     @Override
     protected void onCreate (Bundle savedInstanceStudent) {
         super.onCreate(savedInstanceStudent);
@@ -27,9 +33,54 @@ public class MainActivity extends AppCompatActivity {
             students.add(new Student("EEEEE", 5));
         }
 
-        ListView studentList = (ListView)findViewById(R.id.studentList);
-        StudentAdapter adapter = new StudentAdapter(this, R.layout.list_item, students);
+        final ListView studentList = (ListView)findViewById(R.id.studentList);
+        adapter = new StudentAdapter(this, R.layout.list_item, students);
         studentList.setAdapter(adapter);
+
+        nameEditText = findViewById(R.id.nameOfStudent);
+        markEditText = findViewById(R.id.markOfStudent);
+
+        studentList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                studentItem = adapter.getItem(position);
+                nameEditText.setText(studentItem.getName());
+                markEditText.setText(Integer.toString(studentItem.getMark()));
+            }
+        });
+        for(int i = 0; i < students.size(); i++) {
+            students.remove(studentItem);
+        }
     }
 
+    /*public void onButtonPressedRemove(View view) {
+        for (Student item : students) {
+            if (item.getName().equals(nameEditText.getText())) {
+                students.remove(studentList.(item));
+            } else {
+                nameEditText.setText("");
+            }
+        }
+        ListView studentList = (ListView)findViewById(R.id.studentList);
+        adapter = new StudentAdapter(this, R.layout.list_item, students);
+        studentList.setAdapter(adapter);
+    }*/
+
+    public void onButtonPressedRemove(View view) {
+        for(int i = 0; i < students.size(); i++) {
+            students.remove(studentItem);
+        }
+            ListView studentList = (ListView)findViewById(R.id.studentList);
+            adapter = new StudentAdapter(this, R.layout.list_item, students);
+            studentList.setAdapter(adapter);
+        nameEditText.setText("");
+        markEditText.setText("");
+    }
+
+    public void onButtonPressedAdd(View view) {
+        students.add(new Student(nameEditText.getText().toString(), Integer.valueOf(markEditText.getText().toString())));
+        ListView studentList = (ListView)findViewById(R.id.studentList);
+        adapter = new StudentAdapter(this, R.layout.list_item, students);
+        studentList.setAdapter(adapter);
+    }
 }
